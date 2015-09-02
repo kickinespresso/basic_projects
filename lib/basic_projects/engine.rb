c0b97d6d8adbeb060271c87b40d5f67276fdb747
Chris Desch <cdesch@gmail.com>
2015-09-02 08:07:18 -0400

    Updating Rspec
    Signed-off-by: Chris Desch <cdesch@gmail.com>

--- lib/basic_projects/engine.rb
+++ lib/basic_projects/engine.rb
@@ -1,5 +1,21 @@
+require 'bootstrap-sass'
+require 'friendly_id'
+require 'simple_form'
+require 'activeadmin'
+
 module BasicProjects
   class Engine < ::Rails::Engine
+    require 'jquery-rails'
+    require 'jquery-ui-rails'
+    require 'ckeditor'
+
     isolate_namespace BasicProjects
+
+
+    config.generators do |g|
+      g.test_framework      :rspec
+      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+    end
+
   end
 end
