62c1a47720aa5e5427d5d39713271738765164f9
Chris Desch <cdesch@gmail.com>
2015-09-27 18:43:04 -0400

    Updating
    Signed-off-by: Chris Desch <cdesch@gmail.com>

--- app/helpers/basic_projects/projects_helper.rb
+++ app/helpers/basic_projects/projects_helper.rb
@@ -1,4 +1,12 @@
 module BasicProjects
   module ProjectsHelper
+    def show_project_bg(project)
+      if project.image.blank?
+        #FIXME... static image
+        "background-image: url('#{image_path "basic_project/post-thumb03.jpg"}')"
+      else
+        "background-image: url('#{project.image_url(:thumb)}')"
+      end
+    end
   end
 end
